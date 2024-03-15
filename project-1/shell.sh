f8b9e81f98310ee202e44d7654800eaaf7b30d55
charovatzbassar <carovac.basar@gmail.com>
2024-03-15 10:18:33 +0100

    remove comments

--- project-1/shell.sh
+++ project-1/shell.sh
@@ -1,10 +1,9 @@
-# Function to handle Ctrl+C
 ctrl_c() {
     echo ""
     exit 1
 }
 
-# Trap Ctrl+C and call the ctrl_c function
+
 trap ctrl_c SIGINT
 
 while true; do
@@ -16,6 +15,5 @@ while true; do
         break
     fi
 
-    # Execute the command
     eval $input
 done
